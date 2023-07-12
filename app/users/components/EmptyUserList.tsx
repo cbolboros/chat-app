8bed6c4d65d0d9cf610f724fbc8defded6b1ebcd
Ciprian Bolboros <cbolboros@gmail.com>
2023-07-12 19:48:05 +0300

    and it's gone

--- app/users/components/EmptyUserList.tsx
+++
@@ -1,9 +0,0 @@
-const EmptyUserList = () => {
-  return (
-    <div className="text-gray-400">
-      <div>No other users are created.......</div>
-    </div>
-  );
-};
-
-export default EmptyUserList;
